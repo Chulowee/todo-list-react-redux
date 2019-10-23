f965d48108dc3b17e3345b6dc9cd9082d64aa254
aklagdameo <lagdameo.aaronkm@gmail.com>
2019-10-23 08:32:54 +0800

    [Aaron & Chloe] adjusted creation of store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {Provider} from "react-redux";
 import rootReducer from "./reducers";
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
