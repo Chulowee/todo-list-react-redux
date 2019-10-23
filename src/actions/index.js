3db5f5f5d73f9093cf1adbe8132c1a659d2cf1e4
aklagdameo <lagdameo.aaronkm@gmail.com>
2019-10-23 08:33:47 +0800

    [Aaron & Chloe] adjusted add-todo action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
-export const addTodo = title => ({
-    type: 'ADD_TODO',
-    id: generateId(),
-    title: title
-});
+export const addTodo = (title) => {
+    return (dispatch) => (
+        dispatch({
+            type: 'ADD_TODO',
+            id: generateId(),
+            title: title
+        })
+    )
+};
 
 const generateId = () => {
     return new Date().getTime() + Math.random();
